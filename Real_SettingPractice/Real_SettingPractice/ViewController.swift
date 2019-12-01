a3fef8671f444419907396a45060aa5518cb914d
JeongMinHo <wjdalsgh95@nate.com>
2019-12-02 00:51:00 +0900

    -중간 저장-

--- Real_SettingPractice/Real_SettingPractice/ViewController.swift
+++ Real_SettingPractice/Real_SettingPractice/ViewController.swift
@@ -1,66 +1,70 @@
 //
 //  ViewController.swift
-//  TableViewPractice
+//  Real_SettingPractice
 //
-//  Created by jeongminho on 2019/11/28.
+//  Created by jeongminho on 2019/12/01.
 //  Copyright © 2019 jeongminho. All rights reserved.
 //
 
 import UIKit
 
-enum SettingRow: CaseIterable {
+enum SettingRow {
     case notice
     case ask
-    case appVersion
-
+    
     var title: String {
         switch self {
         case .notice: return "공지사항"
         case .ask: return "앱문의/건의"
-        case .appVersion: return "앱 버전"
         }
     }
-
+    
     var identifier: String {
         switch self {
         case .notice: return "1"
         case .ask: return "2"
-        case .appVersion: return "1"
         }
     }
 }
 
 class ViewController: UIViewController {
     
-    var models: [SettingRow] = SettingRow.allCases
-
+    var models: [SettingRow] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+
 }
 
+    // MARK: - Data Source
+
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let dequeued = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath)
+        
         guard let cell = dequeued as? SettingCell else {
             return dequeued
         }
+        
         cell.model = models[indexPath.row]
         cell.moveButtonTapped = { [weak self] in
             guard let self = self else { return }
             self.performSegue(withIdentifier: self.models[indexPath.row].identifier, sender: nil)
         }
+        
         return cell
+        
     }
 }
 
 class SettingCell: UITableViewCell {
-    
     @IBOutlet weak var title: UILabel!
     
     var moveButtonTapped: (() -> Void)?
@@ -74,4 +78,5 @@ class SettingCell: UITableViewCell {
     @IBAction func moveButtonTapped(_ sender: UIButton) {
         moveButtonTapped?()
     }
+  
 }
