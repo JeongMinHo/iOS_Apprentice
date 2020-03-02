efe8c19a0e7f5c532f4f4f0d145aa2c5c077e8c9
JeongMinHo <wjdalsgh95@nate.com>
2020-03-02 17:30:30 +0900

    StoreSearch: Initial commit

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by jeongminho on 2020/03/02.
+//  Copyright © 2020 jeongminho. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
