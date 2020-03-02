f1f99d34994bbaa8a7cb50250eae24f68e3b890b
JeongMinHo <wjdalsgh95@nate.com>
2020-03-02 20:39:10 +0900

    StoreSearch - Add a search bar and table view

---
+++ StoreSearch/StoreSearch/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by jeongminho on 2020/03/02.
+//  Copyright © 2020 jeongminho. All rights reserved.
+//
+
+import UIKit
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
